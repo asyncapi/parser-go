a2b98a9e27c23096d051bcc4101003ed499b8bb1
fmvilas <fmvilas@gmail.com>
2019-02-24 21:21:37 +0100

    Add Marshal methods and more tests

--- models/info.go
+++ models/info.go
@@ -6,13 +6,13 @@ import (
 
 // Info maps AsyncAPI info object
 type Info struct {
-	ExtensionProps ExtensionProps
-	Title          string   `json:"title,omitempty"`
-	Description    string   `json:"description,omitempty"`
-	TermsOfService string   `json:"termsOfService,omitempty"`
-	Contact        *Contact `json:"contact,omitempty"`
-	License        *License `json:"license,omitempty"`
-	Version        string   `json:"version,omitempty"`
+	ExtensionProps ExtensionProps `json:"-"`
+	Title          string         `json:"title,omitempty"`
+	Description    string         `json:"description,omitempty"`
+	TermsOfService string         `json:"termsOfService,omitempty"`
+	Contact        *Contact       `json:"contact,omitempty"`
+	License        *License       `json:"license,omitempty"`
+	Version        string         `json:"version,omitempty"`
 }
 
 // Unmarshal unmarshals JSON
@@ -20,12 +20,17 @@ func (value *Info) Unmarshal(data []byte) error {
 	return json.Unmarshal(data, value)
 }
 
+// Marshal marshals JSON
+func (value *Info) Marshal() ([]byte, error) {
+	return json.Marshal(value)
+}
+
 // Contact maps AsyncAPI info.contact object
 type Contact struct {
-	ExtensionProps ExtensionProps
-	Name           string `json:"name,omitempty"`
-	URL            string `json:"url,omitempty"`
-	Email          string `json:"email,omitempty"`
+	ExtensionProps ExtensionProps `json:"-"`
+	Name           string         `json:"name,omitempty"`
+	URL            string         `json:"url,omitempty"`
+	Email          string         `json:"email,omitempty"`
 }
 
 // Unmarshal unmarshals JSON
@@ -33,14 +38,24 @@ func (value *Contact) Unmarshal(data []byte) error {
 	return json.Unmarshal(data, value)
 }
 
+// Marshal marshals JSON
+func (value *Contact) Marshal() ([]byte, error) {
+	return json.Marshal(value)
+}
+
 // License maps AsyncAPI info.license object
 type License struct {
-	ExtensionProps ExtensionProps
-	Name           string `json:"name,omitempty"`
-	URL            string `json:"url,omitempty"`
+	ExtensionProps ExtensionProps `json:"-"`
+	Name           string         `json:"name,omitempty"`
+	URL            string         `json:"url,omitempty"`
 }
 
 // Unmarshal unmarshals JSON
 func (value *License) Unmarshal(data []byte) error {
 	return json.Unmarshal(data, value)
+}
+
+// Marshal marshals JSON
+func (value *License) Marshal() ([]byte, error) {
+	return json.Marshal(value)
 }
--- models/info_test.go
+++ models/info_test.go
@@ -13,3 +13,55 @@ func TestInfoUnmarshal(t *testing.T) {
 	assert.Assert(t, is.Nil(err))
 	assert.Equal(t, len(info.ExtensionProps.Extensions), 0)
 }
+
+func TestInfoMarshal(t *testing.T) {
+	info := Info{
+		Title:   "My API",
+		Version: "1.0.0",
+	}
+	result, err := info.Marshal()
+	assert.Assert(t, is.Nil(err))
+	assert.Equal(t, string(result), `{"title":"My API","version":"1.0.0"}`)
+}
+
+func TestInfoContactUnmarshal(t *testing.T) {
+	info := Info{}
+	err := info.Unmarshal([]byte(`{"title":"my API", "contact": { "name": "Fran" } }`))
+	assert.Assert(t, is.Nil(err))
+	assert.Equal(t, len(info.ExtensionProps.Extensions), 0)
+	assert.Equal(t, info.Contact.Name, "Fran")
+}
+
+func TestInfoContactMarshal(t *testing.T) {
+	info := Info{
+		Title:   "My API",
+		Version: "1.0.0",
+		Contact: &Contact{
+			Name: "Fran",
+		},
+	}
+	result, err := info.Marshal()
+	assert.Assert(t, is.Nil(err))
+	assert.Equal(t, string(result), `{"title":"My API","contact":{"name":"Fran"},"version":"1.0.0"}`)
+}
+
+func TestInfoLicenseUnmarshal(t *testing.T) {
+	info := Info{}
+	err := info.Unmarshal([]byte(`{"title":"my API", "license": { "name": "Apache" } }`))
+	assert.Assert(t, is.Nil(err))
+	assert.Equal(t, len(info.ExtensionProps.Extensions), 0)
+	assert.Equal(t, info.License.Name, "Apache")
+}
+
+func TestInfoLicenseMarshal(t *testing.T) {
+	info := Info{
+		Title:   "My API",
+		Version: "1.0.0",
+		License: &License{
+			Name: "Apache",
+		},
+	}
+	result, err := info.Marshal()
+	assert.Assert(t, is.Nil(err))
+	assert.Equal(t, string(result), `{"title":"My API","license":{"name":"Apache"},"version":"1.0.0"}`)
+}
