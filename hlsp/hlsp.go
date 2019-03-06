012661f5c510782e32217798cea3b3d3b638994f
fmvilas <fmvilas@gmail.com>
2019-03-06 21:18:56 +0100

    Separate Parse and ParseJSON for better performance option. Add unit tests.

--- hlsp/hlsp.go
+++ hlsp/hlsp.go
@@ -2,43 +2,30 @@ package hlsp
 
 import (
 	"encoding/json"
-	"fmt"
 
 	"github.com/ghodss/yaml"
 	"github.com/xeipuuv/gojsonschema"
 )
 
-// ParserError is custom struct to hold different error types of the parser
-type ParserError struct {
-	errorMessage  string
-	parsingErrors []gojsonschema.ResultError
-}
-
-// Error returns the error message.
-func (v *ParserError) Error() string {
-	return v.errorMessage
-}
-
-// ParsingErrors returns the errors that occurred while parsing the AsyncAPI document.
-func (v *ParserError) ParsingErrors() []gojsonschema.ResultError {
-	return v.parsingErrors
-}
-
-// Parse receives either a YAML or JSON AsyncAPI document.
-// It parses the document and checks if it's valid AsyncAPI.
-// Skips specification extensions and schemas validation.
-// If validation fails, the Parser/Validator should trigger an error.
-// Produces a beautified version of the document in JSON Schema Draft 07.
-func Parse(AsyncAPI []byte) (json.RawMessage, *ParserError) {
-	schemaLoader := gojsonschema.NewReferenceLoader("file://../asyncapi/2.0.0/schema.json")
-	AsyncAPI, err := convertFromYAMLtoJSON(AsyncAPI)
+// Parse receives either a YAML or JSON AsyncAPI document, and tries to parse it.
+func Parse(yamlOrJSONDocument []byte) (json.RawMessage, *ParserError) {
+	jsonDocument, err := yaml.YAMLToJSON(yamlOrJSONDocument)
 	if err != nil {
 		return nil, &ParserError{
 			errorMessage: err.Error(),
 		}
 	}
+	return ParseJSON(jsonDocument)
+}
 
-	documentLoader := gojsonschema.NewBytesLoader(AsyncAPI)
+// ParseJSON receives a JSON AsyncAPI document.
+// It parses the document and checks if it's valid AsyncAPI.
+// Skips specification extensions and schemas validation.
+// If validation fails, the Parser/Validator should trigger an error.
+// Produces a beautified version of the document in JSON Schema Draft 07.
+func ParseJSON(jsonDocument []byte) (json.RawMessage, *ParserError) {
+	schemaLoader := gojsonschema.NewReferenceLoader("file://../asyncapi/2.0.0/schema.json")
+	documentLoader := gojsonschema.NewBytesLoader(jsonDocument)
 
 	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
 	if err != nil {
@@ -48,28 +35,11 @@ func Parse(AsyncAPI []byte) (json.RawMessage, *ParserError) {
 	}
 
 	if result.Valid() {
-		return AsyncAPI, nil
+		return jsonDocument, nil
 	}
 
-	return AsyncAPI, &ParserError{
-		errorMessage:  "[Invalid AsyncAPI document] Check out .ParsingErrors() for more information.",
+	return jsonDocument, &ParserError{
+		errorMessage:  "[Invalid AsyncAPI document] Check out err.ParsingErrors() for more information.",
 		parsingErrors: result.Errors(),
 	}
-}
-
-func convertToJSON(doc []byte) ([]byte, error) {
-	convertedDoc, err := convertFromYAMLtoJSON(doc)
-	if err != nil {
-		return convertedDoc, nil
-	}
-
-	return nil, fmt.Errorf("[Unsupported document format] Supported formats are: JSON or YAML")
-}
-
-func isJSON(doc string) bool {
-	return false
-}
-
-func convertFromYAMLtoJSON(doc []byte) ([]byte, error) {
-	return yaml.YAMLToJSON(doc)
 }
--- hlsp/hlsp_test.go
+++ hlsp/hlsp_test.go
@@ -1,42 +1,111 @@
 package hlsp
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"os"
 	"testing"
 
-	"github.com/asyncapi/parser/models"
+	"gotest.tools/assert"
+	is "gotest.tools/assert/cmp"
 )
 
-func TestA(t *testing.T) {
-	yamlFile, err := os.Open("../asyncapi/2.0.0/example.yaml")
-	if err != nil {
-		t.Log(err)
-	}
-	defer yamlFile.Close()
+func TestParse(t *testing.T) {
+	asyncapi := []byte(`
+asyncapi: '2.0.0'
+id: myapi
+info:
+  title: My API
+  version: '1.0.0'
+channels: {}`)
 
-	fileBytes, err := ioutil.ReadAll(yamlFile)
-	if err != nil {
-		t.Log(err)
-	}
+	jsonDocument, err := Parse(asyncapi)
+	t.Log(err)
+	// assert.Assert(t, is.Nil(err))
+	assert.Equal(t, string(jsonDocument), `{"asyncapi":"2.0.0","channels":{},"id":"myapi","info":{"title":"My API","version":"1.0.0"}}`)
+}
+func TestParseWithEmptyYAML(t *testing.T) {
+	asyncapi := []byte(``)
 
-	jsonFile, e := Parse(fileBytes)
-	if e != nil {
-		t.Log(e.Error())
-		t.Log(e.ParsingErrors())
-	}
+	jsonDocument, err := ParseJSON(asyncapi)
+	assert.Equal(t, err.Error(), "EOF")
+	assert.Equal(t, len(err.ParsingErrors()), 0)
+	assert.Equal(t, string(jsonDocument), ``)
+}
+func TestParseWithInvalidDocument(t *testing.T) {
+	asyncapi := []byte(`
+asyncapi: '2.0.0'
+info:
+  title: My API
+  version: '1.0.0'
+channels: {}`)
 
-	t.Log(string(jsonFile))
+	jsonDocument, err := Parse(asyncapi)
+	assert.Equal(t, err.Error(), "[Invalid AsyncAPI document] Check out err.ParsingErrors() for more information.")
+	assert.Equal(t, len(err.ParsingErrors()), 1)
+	assert.Equal(t, err.ParsingErrors()[0].Details()["property"], "id")
+	assert.Equal(t, string(jsonDocument), `{"asyncapi":"2.0.0","channels":{},"info":{"title":"My API","version":"1.0.0"}}`)
+}
 
-	var AsyncAPI models.AsyncapiDocument
-	err = json.Unmarshal(jsonFile, &AsyncAPI)
-	if err != nil {
-		t.Log(err)
-	}
-	j, _ := AsyncAPI.Channels["event/{streetlightId}/lighting/measured"].Subscribe.MarshalJSON()
-	t.Log(string(j))
+func TestParseJSON(t *testing.T) {
+	asyncapi := []byte(`{
+		"asyncapi": "2.0.0",
+		"id": "myapi",
+		"info": {
+			"title": "My API",
+			"version": "1.0.0"
+		},
+		"channels": {}
+	}`)
 
-	// j, err := json.Marshal(AsyncAPI.Channels)
-	// t.Log(string(j))
+	jsonDocument, err := ParseJSON(asyncapi)
+	assert.Assert(t, is.Nil(err))
+	assert.Equal(t, string(jsonDocument), `{
+		"asyncapi": "2.0.0",
+		"id": "myapi",
+		"info": {
+			"title": "My API",
+			"version": "1.0.0"
+		},
+		"channels": {}
+	}`)
+}
+
+func TestParseJSONWithInvalidJSON(t *testing.T) {
+	asyncapi := []byte(`"`)
+
+	jsonDocument, err := ParseJSON(asyncapi)
+	assert.Equal(t, err.Error(), "unexpected EOF")
+	assert.Equal(t, len(err.ParsingErrors()), 0)
+	assert.Equal(t, string(jsonDocument), ``)
+}
+
+func TestParseJSONWithEmptyJSON(t *testing.T) {
+	asyncapi := []byte(``)
+
+	jsonDocument, err := ParseJSON(asyncapi)
+	assert.Equal(t, err.Error(), "EOF")
+	assert.Equal(t, len(err.ParsingErrors()), 0)
+	assert.Equal(t, string(jsonDocument), ``)
+}
+
+func TestParseJSONWithInvalidDocument(t *testing.T) {
+	asyncapi := []byte(`{
+		"asyncapi": "2.0.0",
+		"info": {
+			"title": "My API",
+			"version": "1.0.0"
+		},
+		"channels": {}
+	}`)
+
+	jsonDocument, err := ParseJSON(asyncapi)
+	assert.Equal(t, err.Error(), "[Invalid AsyncAPI document] Check out err.ParsingErrors() for more information.")
+	assert.Equal(t, len(err.ParsingErrors()), 1)
+	assert.Equal(t, err.ParsingErrors()[0].Details()["property"], "id")
+	assert.Equal(t, string(jsonDocument), `{
+		"asyncapi": "2.0.0",
+		"info": {
+			"title": "My API",
+			"version": "1.0.0"
+		},
+		"channels": {}
+	}`)
 }
