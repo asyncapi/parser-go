34e8b235d08ab618de978ed2c221746604c3cd75
fmvilas <fmvilas@gmail.com>
2019-03-18 20:40:15 +0100

    Make parser return json but keep code for future

--- parser.go
+++ parser.go
@@ -4,24 +4,34 @@ import (
 	"encoding/json"
 
 	"github.com/asyncapi/parser/hlsp"
-	"github.com/asyncapi/parser/models"
 )
 
 // Parse receives either a YAML or JSON AsyncAPI document, and tries to parse it.
-func Parse(yamlOrJSONDocument []byte) (*models.ParsedAsyncAPI, *hlsp.ParserError) {
+func Parse(yamlOrJSONDocument []byte) (json.RawMessage, *hlsp.ParserError) {
 	doc, err := hlsp.Parse(yamlOrJSONDocument)
 
 	if err != nil {
 		return nil, err
 	}
 
-	var parsedAsyncAPI models.ParsedAsyncAPI
-	e := json.Unmarshal(doc, &parsedAsyncAPI)
-	if e != nil {
-		return nil, &hlsp.ParserError{
-			ErrorMessage: e.Error(),
-		}
-	}
-
-	return &parsedAsyncAPI, nil
+	return doc, nil
 }
+
+// Parse receives either a YAML or JSON AsyncAPI document, and tries to parse it.
+// func Parse(yamlOrJSONDocument []byte) (*models.ParsedAsyncAPI, *hlsp.ParserError) {
+// 	doc, err := hlsp.Parse(yamlOrJSONDocument)
+
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	var parsedAsyncAPI models.ParsedAsyncAPI
+// 	e := json.Unmarshal(doc, &parsedAsyncAPI)
+// 	if e != nil {
+// 		return nil, &hlsp.ParserError{
+// 			ErrorMessage: e.Error(),
+// 		}
+// 	}
+
+// 	return &parsedAsyncAPI, nil
+// }
