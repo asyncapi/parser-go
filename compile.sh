f2fba687448f8637847a14718a28a3447261c621
fmvilas <fmvilas@gmail.com>
2019-04-10 20:03:21 +0200

    Upgrade compilation to Windows 6.0

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xgo -v -x -pkg=cparser -out bin/cparser -buildmode=c-shared -targets="windows/amd64,darwin/amd64,linux/amd64" github.com/asyncapi/parser
+xgo -v -x -pkg=cparser -out bin/cparser -buildmode=c-shared -targets="windows-6.0/amd64,darwin/amd64,linux/amd64" github.com/asyncapi/parser
