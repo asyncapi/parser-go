77d4a708c50191f92db2bcffe138e4d64bc25d9f
fmvilas <fmvilas@gmail.com>
2019-04-21 22:35:47 +0200

    Do not compile by default

--- ci/compile.sh
+++ ci/compile.sh
@@ -1,17 +1,14 @@
-if [ "$TRAVIS_OS_NAME" = "windows" ]; then
-    echo "This is windows!"
-
-    # if [[ $string == *"My long"* ]]; then
-    #     echo "It's there!"
-    # fi
-
+if [[ $TRAVIS_COMMIT_MESSAGE == *"[compile]"* ]]; then
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
-
     git checkout $TRAVIS_BRANCH
+
+  if [ "$TRAVIS_OS_NAME" = "windows" ]; then
     CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o bin/cparser-windows-amd64.dll -buildmode=c-shared cparser/cparser.go
-    git add bin
-    git commit -m "[skip ci] Travis: Add Windows binaries."
-    git remote add origin-ci https://${GH_TOKEN}@github.com/asyncapi/parser.git
-    git push --set-upstream origin-ci $TRAVIS_BRANCH
+  fi
+
+  git add bin
+  git commit -m "[skip ci] Travis: Add $TRAVIS_OS_NAME binaries."
+  git remote add origin-ci https://${GH_TOKEN}@github.com/asyncapi/parser.git
+  git push --set-upstream origin-ci $TRAVIS_BRANCH
 fi
